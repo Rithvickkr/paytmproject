30d9cca6304eeb4f313e880df17eea6e208506fe
Rithvickkr <12216061@nitkkr.ac.in>
2024-04-25 16:54:37 +0530

    typecheck

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,38 +1,48 @@
-import { OnRampTransactions } from "../../../components/OnRampTransaction";
 import { getOnRampTransactions } from "../transfer/page";
+import { OnRampStatus } from "@prisma/client";
 export default async function () {
+   type ty ={
+        time: Date;
+        amount: number;
+        status: OnRampStatus;
+        provider: string;
+   }
   const transactions = await getOnRampTransactions();
   return (
     <div>
       <div className=" ">
         <div className="text-4xl text-blue-500 pt-8 mb-8 font-bold">
-         Recent transactions
+          Recent transactions
         </div>
         <div className="pt-2">
-            {transactions.map(t => (
-                <div className="flex justify-between" key={t.time.toString()}>
-                    <div>
-                        <div className="text-xl text-white">
-                            Received INR
-                        </div>
-                        <div className=" text-s text-white">
-                            {t.time.toDateString()}
-                        </div>
-                        <div className={`${
-                            t.status === "Success" ? "text-green-600" :
-                            t.status === "Processing" ? "text-blue-500" :
-                            "text-red-700"
-                        } text-l`}>
-                            {t.status === "Success" ? "Success" :
-                            t.status === "Processing" ? "Processing" :
-                            "Failed"}
-                        </div>
-                    </div>
-                    <div className="flex flex-col justify-center text-white text-xl">
-                        + Rs {t.amount / 100}
-                    </div>
+          {transactions.map((t:ty) => (
+            <div className="flex justify-between" key={t.time.toString()}>
+              <div>
+                <div className="text-xl text-white">Received INR</div>
+                <div className=" text-s text-white">
+                  {t.time.toDateString()}
                 </div>
-            ))}
+                <div
+                  className={`${
+                    t.status === "Success"
+                      ? "text-green-600"
+                      : t.status === "Processing"
+                        ? "text-blue-500"
+                        : "text-red-700"
+                  } text-l`}
+                >
+                  {t.status === "Success"
+                    ? "Success"
+                    : t.status === "Processing"
+                      ? "Processing"
+                      : "Failed"}
+                </div>
+              </div>
+              <div className="flex flex-col justify-center text-white text-xl">
+                + Rs {t.amount / 100}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -4,8 +4,10 @@ import { BalanceCard } from "../../../components/BalanceCard";
 import { OnRampTransactions } from "../../../components/OnRampTransaction";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../lib/auth";
+import { OnRampStatus } from "@prisma/client";
 
  export async function getBalance() {
+   
     const session = await getServerSession(authOptions);
     const balance = await prisma.balance.findFirst({
         where: {
@@ -19,13 +21,22 @@ import { authOptions } from "../../lib/auth";
 }
 
 export async function getOnRampTransactions() {
+    type tp= {
+        id: number;
+        status: OnRampStatus;
+        token: string;
+        provider: string;
+        amount: number;
+        startTime: Date;
+        userId: number;
+    }
     const session = await getServerSession(authOptions);
     const txns = await prisma.onRampTransaction.findMany({//array comes in this
         where: {
             userId: Number(session?.user?.id)
         }
     });
-    return txns.map(t => ({
+    return txns.map((t:tp)=> ({
         time: t.startTime,
         amount: t.amount,
         status: t.status,
