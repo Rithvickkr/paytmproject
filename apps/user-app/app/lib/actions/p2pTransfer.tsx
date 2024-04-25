acb574d22815d8cca076e678123de80dc4af93d3
Rithvickkr <12216061@nitkkr.ac.in>
2024-04-25 17:36:34 +0530

    edits

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -3,7 +3,11 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 
+
+
+
 export async function p2pTransfer(to: string, amount: number) {
+ 
     const session = await getServerSession(authOptions);
     const from = session?.user?.id;
     if (!from) {
@@ -22,7 +26,8 @@ export async function p2pTransfer(to: string, amount: number) {
             message: "User not found"
         }
     }
-    await prisma.$transaction(async (tx) => {
+
+    await prisma.$transaction(async (tx: any): Promise<void> => {
         await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
         const fromBalance = await tx.balance.findUnique({
             where: { userId: Number(from) },
