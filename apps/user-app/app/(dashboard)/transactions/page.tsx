14751deb63cf935ddf647e44fd331faa6c40f1a1
Rithvickkr <12216061@nitkkr.ac.in>
2024-04-29 20:03:32 +0530

    Fix p2pTransfer function to handle errors and update balances correctly

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -13,6 +13,7 @@ export default async function () {
     provider: string;
   };
   const transactions = await getOnRampTransactions();
+
   async function getusertransactions(from: number) {
     const getinfo: any = await prisma.transaction.findMany({
       where: {
@@ -40,90 +41,107 @@ export default async function () {
     return userdata;
   }
   const usertransactions: any = await getusertransactions(session.user.id);
+  transactions.reverse();
+  usertransactions[0].reverse();
+
   return (
     <div className="text-center flex-auto overflow-hidden">
       <div className="  h-screen overflow-y-scroll  ">
         <div className="text-4xl text-blue-500 pt-8 mb-8 font-bold">
           Recent transactions
         </div>
-        <div className="pt-2 ml-20">
-          {transactions.map((t: ty) => (
-            <div className="flex justify-between py-3" key={t.time.toString()}>
-              <div>
-                <div className="text-xl text-white">Received INR</div>
-                <div className=" text-s text-white">
-                  {t.time.toDateString()}
-                </div>
-                <div
-                  className={`${
-                    t.status === "Success"
-                      ? "text-green-600"
+        <div className="flex gap-72">
+          <div className="pt-2 ml-20">
+            {transactions.map((t: ty) => (
+              <div
+                className="flex justify-between py-3"
+                key={t.time.toString()}
+              >
+                <div>
+                  <div className="text-xl text-white">Added INR</div>
+                  <div className=" text-s text-white">
+                    {t.time.toDateString()}
+                  </div>
+                  <div className="text-l text-white">{t.provider}</div>
+                  <div
+                    className={`${
+                      t.status === "Success"
+                        ? "text-green-600"
+                        : t.status === "Processing"
+                          ? "text-blue-500"
+                          : "text-red-700"
+                    } text-l`}
+                  >
+                    {t.status === "Success"
+                      ? "Success"
                       : t.status === "Processing"
-                        ? "text-blue-500"
-                        : "text-red-700"
-                  } text-l`}
-                >
-                  {t.status === "Success"
-                    ? "Success"
-                    : t.status === "Processing"
-                      ? "Processing"
-                      : "Failed"}
-                </div>
-              </div>
-              <div className=""></div>
-              <div className="flex flex-col justify-center text-white text-xl mr-28">
-                + Rs {t.amount / 100}
-              </div>
-            </div>
-          ))}
-        </div>
-        {usertransactions[0].length !== 0 && (
-          <div>
-            {usertransactions[0].map((t: any) =>
-              usertransactions[1].map((u: any) => (
-                <div className="pt-2 ml-14" key={t.timestamp.toString()}>
-                  <div className="flex justify-between py-3">
-                    <div>
-                      {t.cash === "out" ? (
-                        <div className="text-xl text-white">Sent INR</div>
-                      ) : (
-                        <div className="text-xl text-white">Received INR</div>
-                      )}
-
-                      {t.cash === "out" ? (
-                        <div className="text-xl text-white">to-{u.name}</div>
-                      ) : (
-                        <div className="text-xl text-white">from-{u.name}</div>
-                      )}
-                      {t.cash === "out" ? (
-                        <div className="text-xl text-white">
-                          number-{u.number}
-                        </div>
-                      ) : (
-                        <div className="text-xl text-white">
-                          number-{u.number}
-                        </div>
-                      )}
-                      <div className="text-s text-white">
-                        {t.timestamp.toDateString()}
-                      </div>
-                      <div className="text-s text-green-500">Success</div>
-                    </div>
-                    {t.cash === "out" ? (
-                      <div className="flex flex-col justify-center mr-28 text-red-600 text-xl">
-                        - Rs {t.amount / 100}
-                      </div>
-                    ) : (
-                      <div className="flex flex-col justify-center mr-28 text-green-500 text-xl">
-                        + Rs {t.amount / 100}
-                      </div>
-                    )}
+                        ? "Processing"
+                        : "Failed"}
                   </div>
                 </div>
-              ))
+                <div className=""></div>
+                <div className="flex flex-col justify-center text-white text-xl mr-28">
+                  + Rs {t.amount / 100}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            {usertransactions[0].length !== 0 && (
+              <div>
+                {usertransactions[0].map((t: any) =>
+                  usertransactions[1].map((u: any) => (
+                    <div className="pt-2 ml-14" key={t.timestamp.toString()}>
+                      <div className="flex justify-between py-3">
+                        <div>
+                          {t.cash === "out" ? (
+                            <div className="text-xl text-white">Sent INR</div>
+                          ) : (
+                            <div className="text-xl text-white">
+                              Received INR
+                            </div>
+                          )}
+
+                          {t.cash === "out" ? (
+                            <div className="text-xl text-white">
+                              to-{u.name}
+                            </div>
+                          ) : (
+                            <div className="text-xl text-white">
+                              from-{u.name}
+                            </div>
+                          )}
+                          {t.cash === "out" ? (
+                            <div className="text-xl text-white">
+                              No-{u.number}
+                            </div>
+                          ) : (
+                            <div className="text-xl text-white">
+                              No-{u.number}
+                            </div>
+                          )}
+                          <div className="text-s text-white">
+                            {t.timestamp.toDateString()}
+                          </div>
+                          <div className="text-s text-green-500">Success</div>
+                        </div>
+                        {t.cash === "out" ? (
+                          <div className="flex flex-col justify-center mr-28 text-red-600 text-xl">
+                            - Rs {t.amount / 100}
+                          </div>
+                        ) : (
+                          <div className="flex flex-col justify-center mr-28 text-green-500 text-xl">
+                            + Rs {t.amount / 100}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  ))
+                )}
+              </div>
             )}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -1,115 +1,103 @@
-"use server"
+"use server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 
-
-
-
-
 export async function p2pTransfer(to: string, amount: number) {
   try {
- 
     const session = await getServerSession(authOptions);
     const from = session?.user?.id;
     if (!from) {
-        return {
-            message: "Error while sending",
-            status:"error"
-        }
+      return {
+        message: "Error while sending",
+        status: "error",
+      };
     }
     const toUser = await prisma.user.findFirst({
-        where: {
-            number: to
-        }
+      where: {
+        number: to,
+      },
     });
 
     if (!toUser) {
-        return {
-            message: "User not found",
-            status:"error"
-        }
+      return {
+        message: "User not found",
+        status: "error",
+      };
     }
 
     await prisma.$transaction(async (tx: any): Promise<void> => {
-  
-        await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
-        const fromBalance = await tx.balance.findUnique({
-            where: { userId: Number(from) },
-          });
-          if (!fromBalance || fromBalance.amount < amount) {
-            throw new Error('Insufficient funds');
-          }
+      await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
+      const fromBalance = await tx.balance.findUnique({
+        where: { userId: Number(from) },
+      });
+      if (!fromBalance || fromBalance.amount < amount) {
+        throw new Error("Insufficient funds");
+      }
 
-          await tx.balance.update({
-            where: { userId: Number(from) },
-            data: { amount: { decrement: amount } },
-          });
-          await tx.Transaction.create({
-            data: {
-             
-              amount: amount,
-              timestamp: new Date(),
-               cash:"out",
-              fromUser: {
-                connect: {
-                  id: Number(from)// Replace with the correct ID of the 'from' user
-                }
-              },
-               
-                toUser: {
-                  connect: {
-                    id: toUser.id// Replace with the correct ID of the 'to' user
-                  }
-                }
-             
+      await tx.balance.update({
+        where: { userId: Number(from) },
+        data: { amount: { decrement: amount } },
+      });
+      await tx.Transaction.create({
+        data: {
+          amount: amount,
+          timestamp: new Date(),
+          cash: "out",
+          fromUser: {
+            connect: {
+              id: Number(from), // Replace with the correct ID of the 'from' user
             },
-          });
+          },
 
-          await tx.balance.update({
-            where: { userId: toUser.id },
-            data: { amount: { increment: amount } },
-          });
-          await tx.Transaction.create({
-            
-            data: {
-             
-              amount: amount,
-              timestamp: new Date(), 
-             
-              cash:"in",
-              
-              fromUser: {
-                connect: {
-                  id: toUser.id
-                }
-              },
-               
-                toUser: {
-                  connect: {
-                    id: Number(from)// Replace with the correct ID of the 'to' user
-                  }
-                }
-             
-              
+          toUser: {
+            connect: {
+              id: toUser.id, // Replace with the correct ID of the 'to' user
             },
-          });
-          await tx.p2pTransfer.create({
-            data: {
-                fromUserId: Number(from),
-                toUserId: toUser.id,
-                amount,
-                timestamp: new Date()
-            }
-          })
+          },
+        },
+      });
+
+      await tx.balance.update({
+        where: { userId: toUser.id },
+        data: { amount: { increment: amount } },
+      });
+      await tx.Transaction.create({
+        data: {
+          amount: amount,
+          timestamp: new Date(),
+
+          cash: "in",
+
+          fromUser: {
+            connect: {
+              id: toUser.id,
+            },
+          },
+
+          toUser: {
+            connect: {
+              id: Number(from), // Replace with the correct ID of the 'to' user
+            },
+          },
+        },
+      });
+      await tx.p2pTransfer.create({
+        data: {
+          fromUserId: Number(from),
+          toUserId: toUser.id,
+          amount,
+          timestamp: new Date(),
+        },
+      });
     });
-    return {  
-        message: "Money sent successfully",
-        status:"success"
-    }
-  }
-  catch (e: any) {
-  
-    return e.message == "Insufficient funds" ? { message: e.message, status: "error" } : { message: "Server error, please try again", status: "error" };
+    return {
+      message: "Money sent successfully",
+      status: "success",
+    };
+  } catch (e: any) {
+    return e.message == "Insufficient funds"
+      ? { message: e.message, status: "error" }
+      : { message: "Server error, please try again later", status: "error" };
   }
 }
